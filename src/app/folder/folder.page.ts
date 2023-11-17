b3de7a7aa98f5edd1cf944d6f800b50a5601b1d7
FranklinJPC <franklin.patino@epn.edu.ec>
2023-11-17 16:08:27 -0500

    First Init

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-folder',
@@ -9,8 +10,13 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+  constructor(public photoService: PhotoService) {}
+    addNewToGallery(){
+      this.photoService.addNewToGallery();
+    }
 
+  
+    
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
