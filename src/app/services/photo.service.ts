8a12514953e8e6ca2574a47d1a1ae71fe973c946
Franklin Patiño <jhoelpatino26@hotmail.com>
2023-11-25 20:53:35 -0500

    Implementacion Album sin diseño

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -8,6 +8,7 @@ import {Preferences} from '@capacitor/preferences'
 export class PhotoService {
 
   constructor() { }
+  public photos: UserPhoto[] = [];
   public async addNewToGallery(){
     // Tomar foto 
     const capturarFoto = await Camera.getPhoto({
@@ -15,5 +16,13 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100
     })
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturarFoto.webPath!
+    })
   }
+}
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
 }
